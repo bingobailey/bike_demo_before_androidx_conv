eb57feaa0db74b03cce78f0b3bad0ccdaf0ad62f
Joe <psimoj@gmail.com>
2018-08-30 09:15:46 -0600

    added background image in login

--- lib/widgets/loginfirebase.dart
+++ lib/widgets/loginfirebase.dart
@@ -16,57 +16,68 @@ class _LoginFirebaseWidgetState extends State<LoginFirebaseWidget> {
 @override 
   Widget build(BuildContext context) {
     // TODO: implement build
-    return new Container(
-       margin: EdgeInsets.all(50.0),
-       child: new SafeArea(
-          child: new Form(
-            
-             child: new SingleChildScrollView(
-               // reverse: true,
-                child: new Column(
-                  children: <Widget>[
-
-                       new Padding( padding: EdgeInsets.only( top: 100.0)),
+   // return new Scaffold(
 
 
-                        // email address
-                        new TextFormField(
-                            keyboardType: TextInputType.emailAddress,
-                            style: new TextStyle( fontSize: 20.0, color: Colors.blue),
-                        ),
+       return new Container(
 
-                        new Padding( padding: EdgeInsets.only( top: 175.0)),
+              margin: EdgeInsets.all(50.0),
+              decoration: BoxDecoration( 
+                  borderRadius: BorderRadius.circular(5.0),
+                  border: Border.all(
+                  width: 2.0, 
+                  color: Colors.black), 
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage('assets/lake.jpg'),
+                       colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.4), BlendMode.dstATop),
+                    ),
+              ),
 
-                        // password
-                        new TextFormField(
-                            keyboardType: TextInputType.text,
-                            style: new TextStyle( fontSize: 20.0, color: Colors.blue),
-                            obscureText: true,
-                        ),
+              child: new Form(
+                child: new SingleChildScrollView(
+                  // reverse: true,
+                    child: new Column(
+                      children: <Widget>[
 
-                       new Padding( padding: EdgeInsets.only( top: 5.0)),
+                          new SizedBox( height: 50.0,),
 
-                        new IconButton( 
-                          icon: new Icon(Icons.input),
-                          onPressed: ()=>{}, 
-                          color: Colors.green,
-                          iconSize: 40.0,
-                        )
+                            // email address
+                            new TextFormField(
+                                keyboardType: TextInputType.emailAddress,
+                                style: new TextStyle( fontSize: 20.0, color: Colors.blue),
+                                   
+                            ),
 
+                            new SizedBox( height: 50.0,),
 
+                            // password
+                            new TextFormField(
+                                keyboardType: TextInputType.text,
+                                style: new TextStyle( fontSize: 20.0, color: Colors.blue),
+                                obscureText: true,
+                            ),
 
-                   ],
+                            new SizedBox( height: 50.0,),
+
+                            new IconButton( 
+                              icon: new Icon(Icons.input),
+                              onPressed: ()=>{}, 
+                              color: Colors.green,
+                              iconSize: 40.0,
+                            )
+                       ],
+                    ),        
                 ),
-               
-              
-             ),
-          ),
-       ),
-    );
+              ),
+          );     
   
-
-
+       
+     // );
+    
   } // build function
+
+
 
 
 

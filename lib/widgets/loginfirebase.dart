e03113631af0f891bc56187a0425dd1f287e77c9
Joe <psimoj@gmail.com>
2018-08-30 16:14:35 -0600

    login changes

--- lib/widgets/loginfirebase.dart
+++ lib/widgets/loginfirebase.dart
@@ -14,66 +14,128 @@ class LoginFirebaseWidget extends StatefulWidget {
 class _LoginFirebaseWidgetState extends State<LoginFirebaseWidget> {
 
 
-@override 
-  Widget build(BuildContext context) {
-  
-       return new Container(
+   @override
+     Widget build(BuildContext context) {
 
-              margin: EdgeInsets.all(50.0),
-              decoration: BoxDecoration( 
-                  borderRadius: BorderRadius.circular(5.0),
-                  border: Border.all(
-                  width: 2.0, 
-                  color: Colors.black), 
-                    image: DecorationImage(
+      return new SafeArea(
+         child: new Container(
+            decoration: new BoxDecoration(
+               image: new DecorationImage(
                       fit: BoxFit.cover,
-                      image: AssetImage('assets/lake.jpg'),
-                       colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.3), BlendMode.dstATop),
-                    ),
-              ),
+                    image: AssetImage('assets/lake.jpg'),
+               )
+            ),
+             child: new Opacity(
+                          child: new Container(
+                            margin: EdgeInsets.only( top: 200.0, bottom: 50.0,  left: 50.0, right: 50.0),
+                            decoration: new BoxDecoration(
+                              color: Colors.white,
+                                borderRadius: BorderRadius.circular(5.0),
+                            ) ,
+                           child: new Form(
+                              child: new SingleChildScrollView(
+                                 child: new Column(
+                                    children: <Widget>[
+                                      buildEmailField(),
 
-              child: new Form(
-                child: new SingleChildScrollView(
-                    child: new Column(
-                      children: <Widget>[
+                                    ],
+                                 ),
+                              ),
+                           ),
+                        ),
+                        opacity: .8,
+             )
 
-                          new FlutterLogo( size: 60.0,),
+          
+           )
+            
+         );
+      
+     } // build method
 
-                          new SizedBox( height: 20.0,),
-                          buildEmailField(),
-                          new SizedBox( height: 20.0,),
-                          buildPasswordField() ,
-                          new SizedBox( height: 30.0,),
-                          buildLoginButton(),
+// @override 
+//   Widget build(BuildContext context) {
+  
+//        return new Container(
 
-                       ],
-                    ),        
-                ),
-              ),
-          );     
+//               margin: EdgeInsets.all(50.0),
+//               decoration: BoxDecoration( 
+//                   borderRadius: BorderRadius.circular(5.0),
+//                   border: Border.all(
+//                   width: 2.0, 
+//                   color: Colors.black), 
+//                     image: DecorationImage(
+//                       fit: BoxFit.cover,
+//                       image: AssetImage('assets/lake.jpg'),
+//                        colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.3), BlendMode.dstATop),
+//                     ),
+//               ),
+
+//               child: new Form(
+//                 child: new SingleChildScrollView(
+//                     child: new Column(
+//                       children: <Widget>[
+
+//                           new FlutterLogo( size: 60.0,),
+
+//                           new SizedBox( height: 20.0,),
+//                           buildEmailField(),
+//                           new SizedBox( height: 20.0,),
+//                           buildPasswordField() ,
+//                           new SizedBox( height: 30.0,),
+//                           buildLoginButton(),
+
+//                        ],
+//                     ),        
+//                 ),
+//               ),
+//           );     
   
        
-     // );
+//      // );
     
-  } // build function
+//   } // build function
 
 
   // Email Address Field
   Widget buildEmailField() {
 
 
-  return new TextFormField(
-        decoration: const InputDecoration(
-          border: UnderlineInputBorder(),
-          filled: true,
-          icon: Icon(Icons.email),
-         // hintText: 'Your email address',
-          labelText: 'E-mail',
-        ),
-        keyboardType: TextInputType.emailAddress,
-        onSaved: (String value) { print(value); },
-        style: new TextStyle( fontSize: 30.0, color: Colors.black),
-      );
+
+  return new Container(
+         margin: EdgeInsets.only( left: 25.0, right: 25.0),
+          //alignment: Alignment.center,
+
+           child: TextFormField(
+            textAlign: TextAlign.center,
+              decoration: const InputDecoration(
+              hintText: 'you@example.com',
+              border: UnderlineInputBorder(),
+                labelText: 'email',
+              ),
+              keyboardType: TextInputType.emailAddress,
+              onSaved: (String value) { print(value); },
+              style: new TextStyle( fontSize: 20.0, color: Colors.black, ),
+            ),
+
+        );
+    
+     
+
+
+
+  // return new TextFormField(
+  //       decoration: const InputDecoration(
+  //         border: UnderlineInputBorder(),
+  //         filled: true,
+  //         icon: Icon(Icons.email),
+  //        // hintText: 'Your email address',
+  //         labelText: 'E-mail',
+  //       ),
+  //       keyboardType: TextInputType.emailAddress,
+  //       onSaved: (String value) { print(value); },
+  //       style: new TextStyle( fontSize: 30.0, color: Colors.black),
+  //     );
 
 
 

b406062aa4b4494ae9edaddbd1389bb3b72657c5
Joe <psimoj@gmail.com>
2018-09-01 14:33:40 -0600

    create account mods

--- lib/widgets/createaccount.dart
+++ lib/widgets/createaccount.dart
@@ -3,52 +3,29 @@ import 'package:flutter/material.dart';
 import '../toolbox/credentials.dart';
 
 
-class _LoginData {
+class _AccountData {
   String email = '';
   String password = '';
+  String username = '';
 }
  
 
-class LoginFirebaseWidget extends StatefulWidget {
+class CreateAccountWidget extends StatefulWidget {
   @override
     State<StatefulWidget> createState() {
-      return new _LoginFirebaseWidgetState();
+      return new _CreateAccountWidgetState();
     }
 }
 
-class _LoginFirebaseWidgetState extends State<LoginFirebaseWidget> {
+class _CreateAccountWidgetState extends State<CreateAccountWidget> {
 
   // Attributes: 
   final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
 
-  _LoginData _loginData = new _LoginData();
+  _AccountData _accountData = new _AccountData();
   Credentials _credentials = new Credentials();
 
-  @override
-    void initState() {
-      // TODO: implement initState
-      super.initState();
 
-      _credentials.fetchProvidersForEmail( email: "psimoj@gmail.com").then((loginResult) {
-          print("providers = ${loginResult.providers.toString()}");
-          print("exceptin = ${loginResult.e}");
-
-      });
-
-
-      // See if user is already logged in 
-      _credentials.getCurrentUser().then((loginResult) {
-        if (loginResult.user!=null) {
-          print("User already logged in user: ${loginResult.user}");
-          print("token: ${loginResult.tokenID}");
-        } else {
-          print("user not logged in");
-          print("initiating login");
-        }
-      });
-
-    }
-  
 
 //                ********** Build Methods ************
 
@@ -59,32 +36,22 @@ class _LoginFirebaseWidgetState extends State<LoginFirebaseWidget> {
 
       return new SafeArea(
          child: new Container(
-            decoration: new BoxDecoration(
-               image: new DecorationImage(
-                      fit: BoxFit.cover,
-                    image: AssetImage('assets/lake.jpg'),
-               )
-            ),
-             child: new Opacity(
-                        child: new Container(
-                          margin: EdgeInsets.only( top: 200.0, bottom: 50.0,  left: 50.0, right: 50.0),
-                          decoration: new BoxDecoration(
-                            color: Colors.white,
-                              borderRadius: BorderRadius.circular(5.0),
-                          ) ,
-                          child: buildLoginForm(),
-                          
-                        ),
-                        opacity: .8,
-             )
-           )
-         );
+                margin: EdgeInsets.only( top: 100.0, bottom: 50.0,  left: 50.0, right: 50.0),
+                decoration: new BoxDecoration(
+                  color: Colors.white,
+                    borderRadius: BorderRadius.circular(5.0),
+                ) ,
+                child: buildAccountForm(),
+                
+          ),
+                  
+        );
       
      } // build method
 
 
   // Build the LoginForm
-  Widget buildLoginForm() {
+  Widget buildAccountForm() {
    return new Form(
           key: _formKey,
           child: new SingleChildScrollView(
@@ -94,9 +61,7 @@ class _LoginFirebaseWidgetState extends State<LoginFirebaseWidget> {
                   new SizedBox( height: 10.0,),
                   buildPasswordField(),
                   new SizedBox( height: 20.0,),
-                  buildLoginButton(),
-                  new Text("------ OR -------"),
-                  buildCreateButton(),
+                  buildSubmitButton(),
                 ],
               ),
           ),
@@ -118,7 +83,7 @@ class _LoginFirebaseWidgetState extends State<LoginFirebaseWidget> {
                   labelText: 'email',
                 ),
                 keyboardType: TextInputType.emailAddress,
-                onSaved: (String value) { _loginData.email = value; },
+                onSaved: (String value) { _accountData.email = value; },
                 style: new TextStyle( fontSize: 20.0, color: Colors.black, ),
                 validator: _validateEmail,
                 
@@ -141,7 +106,7 @@ class _LoginFirebaseWidgetState extends State<LoginFirebaseWidget> {
                   ),
                   keyboardType: TextInputType.text,
                   obscureText: true,
-                  onSaved: (String value) { _loginData.password=value; },
+                  onSaved: (String value) { _accountData.password=value; },
                   style: new TextStyle( fontSize: 20.0, color: Colors.black, ),
                   validator: _validatePassword,
                 ),
@@ -150,28 +115,15 @@ class _LoginFirebaseWidgetState extends State<LoginFirebaseWidget> {
 
   }
 
-
-  // Build login button
-  Widget buildLoginButton() {
-    return new Container(
-          width: 250.0,
-         child:  new RaisedButton(
-          color: Colors.blue,
-            child: new Text("Sign In", style: new TextStyle( fontSize: 20.0),),
-            onPressed: _onLoginPressed,
-        ),
-         
-      );
-  }
             
  // Build login button
-  Widget buildCreateButton() {
+  Widget buildSubmitButton() {
     return new Container(
           width: 250.0,
          child:  new RaisedButton(
           color: Colors.green,
-            child: new Text("Create New Account", style: new TextStyle( fontSize: 20.0),),
-            onPressed: _onCreateAccountPressed,
+            child: new Text("Submit", style: new TextStyle( fontSize: 20.0),),
+            onPressed: _onSubmitPressed,
         ),
          
       );
@@ -183,30 +135,23 @@ class _LoginFirebaseWidgetState extends State<LoginFirebaseWidget> {
   //                    ********  Action Methods *********
 
 
-  // Create Account button
-  void _onCreateAccountPressed() {
-
-    print("pressed it");
-  }
-
-
   // Login button
-  void _onLoginPressed() {
+  void _onSubmitPressed() {
 
     // First validate form, then save if OK
     if (this._formKey.currentState.validate()) {
       _formKey.currentState.save(); // This executes the onSave: methods on each field
 
       print('Printing the login data.');
-      print('Email: ${_loginData.email}');
-      print('Password: ${_loginData.password}');
+      print('Email: ${_accountData.email}');
+      print('Password: ${_accountData.password}');
 
       // ==> This is where we would call the actual login
 
   
       //credentials.createAccount();
 
-      _credentials.signInWithEmailAndPassword( email: _loginData.email, password: _loginData.password).then((loginResult) {
+      _credentials.signInWithEmailAndPassword( email: _accountData.email, password: _accountData.password).then((loginResult) {
         print("user= ${loginResult.user}");
         print("tokenid = ${loginResult.tokenID}");
         print("e = ${loginResult.e}");
